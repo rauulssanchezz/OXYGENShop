09108b2c5c8ea1a034f082d54d526aa4ab647593
rauulssanchezz <rauulssanchezz@gmail.com>
2025-01-20 15:51:51 +0100

    FEAT: Contact Form

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ const main = () => {
     try{
         toggleMenu()
         positionMenu()
+        handleContactForm()
     }catch(error){
         console.error(error)
     }
@@ -36,22 +37,81 @@ function positionMenu() {
     const lists = document.getElementsByClassName('nav-li')
     const returnButton = document.getElementById('return-button')
 
-    if(window.innerWidth >= 1200){
-        for(let i = 0; i < lists.length; i++){
-            lists[i].addEventListener('click', () => {
-                for(let j = 0; j < lists.length; j++){
-                    lists[j].style.border = 'none'
-                }
-                lists[i].style.borderBottom = '2px solid #08A6E4'
-            })
-        }
-
-        returnButton.addEventListener('click', () => {
-            for(let i = 0; i < lists.length; i++){
-                lists[i].style.border = 'none'
+    for(let i = 0; i < lists.length; i++){
+        lists[i].addEventListener('click', () => {
+            for(let j = 0; j < lists.length; j++){
+                lists[j].style.border = 'none'
             }
-            lists[0].style.borderBottom = '2px solid #08A6E4'
+            lists[i].style.borderBottom = '2px solid #08A6E4'
         })
-
     }
+
+    returnButton.addEventListener('click', () => {
+        for(let i = 0; i < lists.length; i++){
+            lists[i].style.border = 'none'
+        }
+        lists[0].style.borderBottom = '2px solid #08A6E4'
+    })
+}
+
+function handleContactForm(){
+    const nameInput = document.getElementById('name')
+    const emailInput = document.getElementById('email')
+    const contactInputButton = document.getElementById('contact-input-button')
+    const checkBoxPrivacity = document.getElementById('privacity')
+
+    let name = ''
+    let email = ''
+    let check = false
+
+    const emailRegex = new RegExp(
+    '^[a-zA-Z0-9._%+-]+@[a-zAZ0-9.-]+\.[a-zA-Z]{2,}$'
+    )
+
+    contactInputButton.addEventListener('click', () => {
+        name = nameInput.value
+        email = emailInput.value
+        check = checkBoxPrivacity.value
+
+        nameInput.style.borderBottom = '2px solid gray'
+        emailInput.style.borderBottom = '2px solid gray'
+        checkBoxPrivacity.style.border = '2px solid gray'
+
+        if(name.length > 2 && name. length < 100 
+            && emailRegex.test(email)
+            && check){
+                alert('Form sent successfully')
+                nameInput.value = ''
+                emailInput.value = ''
+                nameInput.style.borderBottom = '2px solid gray'
+                emailInput.style.borderBottom = '2px solid gray'
+                checkBoxPrivacity.style.border= '2px solid gray'
+
+                fetch('https://jsonplaceholder.typicode.com/posts', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        name: name,
+                        email: email,
+                        check: check
+                    }),
+                    headers: {
+                        'Content-type': 'application/json; charset=UTF-8',
+                    }
+                }).then((response) => response.json())
+                .then((json) => console.log(json))
+
+        }else if(name === '' || (name <= 2 && name >= 100)){
+            alert('Name is required and have to be between 2 and 100 characters')
+            nameInput.style.borderBottom ='2px solid red'
+            return
+        }else if(email === '' || !email.match(emailRegex)){
+            alert('Email is required and have to be a valid email')
+            emailInput.style.borderBottom ='2px solid red'
+            return
+        }else if(!check){
+            alert('You have to accept the privacity policy')
+            checkBoxPrivacity.style.border ='2px solid red'
+            return
+        }
+    })
 }
